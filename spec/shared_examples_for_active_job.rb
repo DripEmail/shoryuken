8c325c32f6332b46f334fe3e5ed6edc806bd146b
Tim Breitkreutz <tim.breitkreutz@drip.com>
2020-10-28 10:11:27 -0400

    allow message attributes to be passed in

--- spec/shared_examples_for_active_job.rb
+++ spec/shared_examples_for_active_job.rb
@@ -39,6 +39,28 @@ RSpec.shared_examples 'active_job_adapters' do
         subject.enqueue(job)
       end
     end
+
+    context 'with additional message attributes' do
+      it 'should combine with activejob attributes' do
+        custom_message_attributes = {
+          'tracer_id' => {
+            string_value: SecureRandom.hex,
+            data_type: 'String'
+          }
+        }
+
+        expect(queue).to receive(:send_message) do |hash|
+          expect(hash[:message_deduplication_id]).to_not be
+          expect(hash[:message_attributes]['shoryuken_class'][:string_value]).to eq(described_class::JobWrapper.to_s)
+          expect(hash[:message_attributes]['shoryuken_class'][:data_type]).to eq("String")
+          expect(hash[:message_attributes]['tracer_id'][:string_value]).to eq(custom_message_attributes['tracer_id'][:string_value])
+          expect(hash[:message_attributes]['tracer_id'][:data_type]).to eq("String")
+        end
+        expect(Shoryuken).to receive(:register_worker).with(job.queue_name, described_class::JobWrapper)
+
+        subject.enqueue(job, message_attributes: custom_message_attributes)
+      end
+    end
   end
 
   describe '#enqueue_at' do
